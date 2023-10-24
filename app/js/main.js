d30dc72df8d44dc4962b6a8b5d92dee449c05c30
oilprint <irynakozlova.k@gmail.com>
2023-10-24 20:52:19 +0300

    Added section hero

--- app/js/main.js
+++ app/js/main.js
@@ -1,52 +1,19 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-
-      //Mobile Menu
-    const burger = document.querySelector('.header__burger');
-
-    console.log('test');
-    const mobileMenu = document.querySelector('.mobile-menu'); 
-    const closeButton = document.querySelector('.mobile-menu__button');
-    const bodyLock = document.querySelector('body'); 
-
-    burger.addEventListener('click', () => {
-      mobileMenu.classList.add('mobile-menu--active'); 
-      bodyLock.classList.add('lock');
-    });
-
-    closeButton.addEventListener('click', () => {
-      mobileMenu.classList.remove('mobile-menu--active'); 
-      bodyLock.classList.remove('lock');
-    });
-
-
-
-    //swiper
-
-    const swiper = new Swiper('.swiper', {
-    
-    slidesPerView: 1,
-    autoplay: {
-      delay: 2000,
-    },
-      loop: true,
-    });
-    
-
-    $(function () {
-      if (window.location.pathname === '/works.html') {
-        var mixer = mixitup('.works__content');
-      }
-    });
-
+const swiper = new Swiper('.swiper', {
+  speed: 1500,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+  // mousewheel: true,
+  autoplay: {
+        delay: 1200,
+        pauseOnMouseEnter: true,
+      },
+  spaceBetween: 18,
+  parallax:true, 
+  keyboard: {
+        enabled: true,
+      },
 });
-
-
-  
-
-
-
-
-
-
-
