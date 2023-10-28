05368004c4b253da4250227e04ca45aeb371568d
oilprint <irynakozlova.k@gmail.com>
2023-10-28 17:57:06 +0300

    bug fexes: branch dev

--- gulpfile.js
+++ gulpfile.js
@@ -38,9 +38,9 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     // 'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/mixitup/dist/mixitup.js',
+    // 'node_modules/mixitup/dist/mixitup.js',
     'node_modules/swiper/swiper-bundle.js',
-    'app/js/modal.js',  
+    // 'app/js/modal.js',  
     'app/js/main.js'  
   ])
   .pipe(concat('main.min.js')) 
