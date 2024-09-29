2472360d24489e2c2d5737e76cdd4fc51278f6cd
oilprint <irynakozlova.k@gmail.com>
2024-09-29 10:19:41 -0400

    completed team section

--- app/js/about.js
+++ app/js/about.js
@@ -2,11 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
 console.log('click');
 
  function removeActiveClasses(elements, className) {
-  
-  
     for (el of elements) {
       el.classList.remove(className);
-console.log('remove');
     };
   };
 
@@ -14,11 +11,14 @@ console.log('remove');
         tabsContentTeam = document.querySelector('.team__content');
 
   function clickOnTeamTabHandler(target) {
+    
+    
     removeActiveClasses(tabsNavTeam.children, 'active');
     removeActiveClasses(tabsContentTeam.children, 'active');
-    // target.classList.add('active');
-    // const contentItemTeam = tabsContentTeam.querySelector(`[data-content="${target.dataset.id}"]`);
-    // contentItemTeam.classList.add('active');
+    console.log(tabsContentTeam.children);
+    target.classList.add('active');
+    const contentItemTeam = tabsContentTeam.querySelector(`[data-content="${target.dataset.id}"]`);
+    contentItemTeam.classList.add('active');
   };
 
   tabsNavTeam?.addEventListener('click', (e) => {
